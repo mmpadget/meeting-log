e139bbfbf224879eb54eab64c6d2998b65876ccf
Michael Padget <mpadget@icloud.com>
2020-03-17 19:54:40 -0700

    Remove comments

--- src/App.js
+++ src/App.js
@@ -42,48 +42,6 @@ class App extends Component {
     })
   }
 
-  // componentDidMount() {
-  //   const ref = firebase.database().ref('user');
-  //   ref.on('value', snapshot => {
-  //     let FBUser = snapshot.val();
-  //     console.log('FBUser: ', FBUser);
-  //     this.setState({ user: FBUser });
-  //   });
-
-    // Result: FBUser: "Bob"
-    // https://firebase.google.com/docs/database/web/read-and-write
-    // var userId = firebase.auth().currentUser.uid;
-    // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-      //   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-      //   console.log(username);
-      // });
-
-    // return firebase.database().ref('/user')
-    //   .once('value')
-    //   .then(function(snapshot) {
-    //     console.log(snapshot.val());
-    // });
-    // Result: 'Bob'
-
-    // const database = firebase.database().ref('/user')
-    //   .once('value')
-    //   .then(function(snapshot) {
-    //     console.log(snapshot.val());
-    //     console.log('database: ', database);
-    // });
-    // Result: 'Bob' (takes a minute)
-    // Result: Promise {status: "pending"}
-
-    // const ref = firebase.database().ref('user');
-    // ref.on('value', function(snapshot) {
-    //   let FBUser = snapshot.val();
-    //   console.log('FBUser: ', FBUser);
-    //   this.setState({ user: FBUser });
-    // });
-    // FBUser: "Bob"
-    // Result: TypeError: null is not an object (evaluating 'this.setState')
-  // }
-
   render() {
     return (
       <div>
@@ -102,30 +60,3 @@ class App extends Component {
 }
 
 export default App;
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
